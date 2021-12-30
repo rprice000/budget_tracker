738b8a0c9d23f9d52d6da34f6db8e6f0dcf52bac
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-12-30 10:10:30 -0600

    testing server file error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const budget_tracker = process.env.budget_tracker || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(budget_tracker, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
