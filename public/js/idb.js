4d807eaaad5acbdc86a6c949392b32459f1256c1
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-12-30 09:41:28 -0600

    fixed idb.js error

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,7 @@ const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createdObjectStore('new_transaction', { autoIncrement: true });
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
