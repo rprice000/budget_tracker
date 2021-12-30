725d736de1d003584e5c4fae2a12ff641ff5af64
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-12-30 10:19:05 -0600

    fixed Mongo Atlas issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const budget_tracker = process.env.budget_tracker || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(budget_tracker, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
